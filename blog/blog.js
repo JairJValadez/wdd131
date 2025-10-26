9ae0c4616fa6f6cbe0f0243a7f8bed4d64535df4
JairValadezA <val20013@byui.edu>
2025-10-25 21:01:40 -0600

    W06 Prove (a lot of changes)

--- blog/blog.js
+++ blog/blog.js
@@ -23,5 +23,72 @@ const articles = [
 		ages: '12-16',
 		genre: 'Fantasy',
 		stars: '⭐⭐⭐⭐'
+	},
+	{
+		id: 3,
+		title: "Belgariad Book One: Pawn of Prophecy",
+		date: "Feb 12, 2022",
+		description:
+		"A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+		imgSrc:
+		"https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+		imgAlt: "Book cover for Pawn of Prophecy",
+		ages: "12-16",
+		genre: "Fantasy",
+		stars: "⭐⭐⭐⭐⭐"
 	}
 ]
+
+//Creates the HTML for the article element, using the hard coded one as a 'template'.
+function createArticleHtml(article) {
+
+    const articleClass = 'review'
+    const figureClass = 'book-cover'
+    const readMoreLink = '#';
+	
+    // This was pretty much the hard coded article (this was interesting to code)
+    return `
+        <article class="${articleClass}">
+            
+            <div class="review-info">
+                <time datetime="${article.date}">${article.date}</time>
+                <p>${article.ages}</p>
+                <p>${article.genre}</p>
+                <div class="rating" aria-label="Book Quality Rating: ${article.stars.length} out of 5 stars">
+                    <span aria-hidden="true">${article.stars}</span>
+                </div>
+            </div>
+
+            <section class="review-content">
+                <h2>${article.title}</h2>
+                
+                <figure class="${figureClass}">
+                    <img 
+                        src="${article.imgSrc}" 
+                        alt="${article.imgAlt}"
+                    >
+                    <figcaption>Book Cover Image</figcaption>
+                </figure>
+                
+                <p class="excerpt">${article.description}</p>
+                
+                <a href="${readMoreLink}" class="read-more-link">Read More...</a>
+            </section>
+        </article>
+    `
+}
+
+//Loops through the articles, creates, and appends
+function displayArticles(articlesList, container) {
+    let theArticles = '';
+
+	articlesList.forEach(article => {
+        const articleHtml = createArticleHtml(article)
+        theArticles += articleHtml;
+    })
+
+	container.innerHTML = theArticles;
+}
+
+const articleCreator = document.querySelector('#article-creator');
+displayArticles(articles, articleCreator);
